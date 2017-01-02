6a9246b9c01ecd344ea14638cbeb1dd16493236e
jisanghoon <246060@naver.com>
2017-01-02 12:50:22 +0900

    merge 테스트

--- src/main/java/kr/co/home/postcode/dao/PostcodeDao.java
+++ src/main/java/kr/co/home/postcode/dao/PostcodeDao.java
@@ -6,5 +6,5 @@ public interface PostcodeDao<T> {
 	T selectByNo(int idx);
 	List<T> selectAll();
 	List<T> selectCodeWithAddressBySidoAndDoro(T t);
-	List<String> selectSido();
+	List<T> selectSido();
 }
